299fd91155f8815b490a2b0041fe2cadae97f8e5
Michael Hrehocik <mrh5766@rit.edu>
2020-10-13 11:50:37 -0400

    Added tags to objects for use with collision, among other things, and added telekinesis.

--- Assets/Scripts/Object.cs
+++ Assets/Scripts/Object.cs
@@ -1,10 +1,26 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Object : MonoBehaviour
 {
-    //Enum to store possible properties of objects
+    public static List<Object> objects; //An easily accessible list of all the objects
+
+    public Player player;
+    /* 
+     * objTag determines what the object can do.
+     * 
+     * 'moveable' - can be moved with telekinesis
+     * 'trap' - teleports the player back to their starting location (placeholder)
+     * 'victory' - the goal of the level
+     */
+    public string objTag;
+
+    public float moveStrength; //The strength of the force that telekinesis applies - could potentially make this different on different objects.
+    private bool isBehingHeld = false;
+    public Rigidbody2D rb;
+    /*//Enum to store possible properties of objects
     // 0 - None
     // 1 - Dangerous
     // 2 - Moveable
@@ -32,16 +48,47 @@ public class Object : MonoBehaviour
     }
     public int properties;
     private ObjectProperties objProperties;
+    */
     public bool isGoal; //Determines whether or not the object is the end goal of the level
     // Start is called before the first frame update
     void Start()
     {
-        objProperties = (ObjectProperties)properties;
+        rb = GetComponent<Rigidbody2D>();
+        tag = objTag;
+        if (objects == null)
+        {
+            objects = new List<Object>();
+        }
+        objects.Add(this);
+        //objProperties = (ObjectProperties)properties;
     }
 
     // Update is called once per frame
     void Update() //Probably need stuff here for pipes
     {
-        
+        if (isBehingHeld) 
+            //Handles the telekinesis - applies a force toward the mouse cursor on the object. 
+            //May need to add something to make sure multiple things can't be held at once if they slightly overlap.
+        {
+            Vector3 mousePos = Input.mousePosition;
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+            isBehingHeld = true;
+            rb.AddForce(moveStrength * (mousePos - this.transform.position));
+        }
+    }
+    private void OnMouseDown() //Checks if the player left clicks on a moveable object in order to apply telekinesis
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (tag == "moveable")
+            {
+                isBehingHeld = true;
+            }
+        }
+    }
+    private void OnMouseUp() //If the player releases the mouse, no longer applies telekinesis
+    {
+        isBehingHeld = false;
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    Vector3 startPos;
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPos = this.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        switch(tag)
+        {
+            case "trap":
+                this.transform.position = startPos; //There'll need to be a ResetLevel() method later - I'm just doing this as a placeholder.
+                this.transform.rotation = Quaternion.Euler(0, 0, 0);
+                break;
+            case "victory":
+                //Occurs when the player reaches the end of the level
+                break;
+            default:
+                break;
+        }
     }
 }
