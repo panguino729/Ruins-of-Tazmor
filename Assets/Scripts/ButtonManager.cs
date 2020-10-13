c07a28d61f1c2787f789ddfd98e21c61c67edfce
Kat <kft3635@rit.edu>
2020-10-13 17:17:29 -0400

    Added UI element placeholders for menus and current spell

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -5,6 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class ButtonManager : MonoBehaviour
 {
+    [SerializeField] private GameObject uIManager;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -20,5 +22,15 @@ public class ButtonManager : MonoBehaviour
     public void OnStart()
     {
         SceneManager.LoadScene("TestScene");
+    }
+    
+    public void OnChangeSpell()
+    {
+        Debug.Log("Change Spells");
+    }
+    
+    public void OnMenu()
+    {
+        uIManager.GetComponent<UIManager>().ButtonPress("pause)");
     }
 }
