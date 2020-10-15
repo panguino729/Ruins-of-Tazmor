e878fa1054f4acb53aaa4ae2c27343622d267d12
Van Edelman <vanedelman@gmail.com>
2020-10-15 14:13:57 -0400

    added door functionality

---
+++ Assets/Door.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Door : MonoBehaviour
+{
+    public Scene levelComplete;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+       if(collision.gameObject.name == "Player")
+            {
+                //SceneManager.SetActiveScene(levelComplete);
+                SceneManager.LoadScene("LevelComplete");
+            }
+       }
+}
