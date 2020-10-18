c79dfd74d0ed3fb5da4b54675689e2820ccc55e1
Michael Hrehocik <mrh5766@rit.edu>
2020-10-18 19:41:44 -0400

    Made pressure plates have feedback when pressed

--- Assets/Scripts/Plate.cs
+++ Assets/Scripts/Plate.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// A plate that 
+/// A plate that is pressed when a player or moveable object interacts with it
 /// </summary>
 public class Plate : Object
 {
     public bool pressed = false;
+    private float initialYScale;
+    private BoxCollider2D collider;
+    private float initialHeight;
+    public float indentHeight; //The % height that the plate changes each frame upon being pressed - should probably be about 0.1
+    GameObject collided;
     // Start is called before the first frame update
     void Start()
     {
+        if(indentHeight == 0)
+        {
+            indentHeight = 0.1f;
+        }
+        initialYScale = transform.localScale.y;
+        collider = GetComponent<BoxCollider2D>();
+        initialHeight = collider.bounds.size.y;
         if (name == null)
         {
             name = "Plate";
@@ -24,12 +36,23 @@ public class Plate : Object
     // Update is called once per frame
     void Update()
     {
-
+        if (pressed && transform.localScale.y > 0.5f * initialYScale)
+        {
+            gameObject.transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y - indentHeight * initialYScale, transform.localScale.z);
+            gameObject.transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y - indentHeight * initialHeight, transform.localPosition.z);
+            collided.gameObject.transform.localPosition = new Vector3(collided.gameObject.transform.localPosition.x, collided.gameObject.transform.localPosition.y - indentHeight * initialHeight, collided.gameObject.transform.localPosition.z);
+        }
+        else if(!pressed && transform.localScale.y < 1.0f * initialYScale)
+        {
+            gameObject.transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y + indentHeight * initialYScale, transform.localScale.z);
+            gameObject.transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y + indentHeight * initialHeight, transform.localPosition.z);
+        }
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.name.Contains("Moveable") || collision.gameObject.name.Contains("Player"))
         {
+            collided = collision.gameObject;
             pressed = true;
         }
     }
