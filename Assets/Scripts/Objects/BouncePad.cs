7e222f15348494e5c4617afae8449228755dc6d9
Michael Hrehocik <mrh5766@rit.edu>
2020-10-20 13:04:47 -0400

    Added more comments and basic error handling

--- Assets/Scripts/Objects/BouncePad.cs
+++ Assets/Scripts/Objects/BouncePad.cs
@@ -44,33 +44,42 @@ public class BouncePad : MonoBehaviour
     {
         if (needsPlates)
         {
+            if(plates == null)
+            {
+                throw new System.Exception("Plates cannot be null if you have needsPlates enabled. " +
+                    "If you don't want to switch the bouncepad on/off with plates, disable needsPlates. " +
+                    "Otherwise, add plates to the list.");
+            }
             for (int i = 0; i < plates.Count; i++)
             {
                 if (!plates[i].pressed)
                 {
-                    if(platesPressed)
+                    if (platesPressed)
                     {
                         justPressedOrUnpressed = true;
                     }
                     platesPressed = false;
                     break;
                 }
-                if(i == plates.Count - 1)
+                if (i == plates.Count - 1)
                 {
-                    if(!platesPressed)
+                    if (!platesPressed)
                     {
                         justPressedOrUnpressed = true;
                     }
                     platesPressed = true;
                 }
             }
+            
         }
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (!needsPlates || (platesEnable && platesPressed) || (!platesEnable && !platesPressed)) //Bounces if the bouncepad: 1. Does not care about plates 
-            //2. Cares about plates, plates enable the pad, and all plates are pressed
-            //3. Cares about plates, plates disable the pad, and not all plates are pressed
+        //Bounces if the bouncepad: 
+        //1. Does not care about plates 
+        //2. Cares about plates, plates enable the pad, and all plates are pressed
+        //3. Cares about plates, plates disable the pad, and not all plates are pressed
+        if (!needsPlates || (platesEnable && platesPressed) || (!platesEnable && !platesPressed)) 
         {
             if (collision.gameObject.name.Contains("Bounceable")) //Upon colliding the bounce pad, apply a force to bounce the object
             {
@@ -81,6 +90,7 @@ public class BouncePad : MonoBehaviour
     }
     private void OnCollisionStay2D(Collision2D collision)
     {
+        //Makes it so that objects sitting on a bouncepad are bounced as soon as the plates allow it to bounce them
         if(justPressedOrUnpressed)
         {
             OnCollisionEnter2D(collision);
