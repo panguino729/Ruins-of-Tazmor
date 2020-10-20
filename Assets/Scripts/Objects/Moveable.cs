46df1501604753d4908e7e019d8616698b6d9d20
Michael Hrehocik <mrh5766@rit.edu>
2020-10-20 16:38:47 -0400

    Set moveable layer in code

--- Assets/Scripts/Objects/Moveable.cs
+++ Assets/Scripts/Objects/Moveable.cs
@@ -9,11 +9,12 @@ public class Moveable : MonoBehaviour
 {
     public float moveStrength; //The strength of the force that telekinesis applies - could potentially make this different on different objects.
     public float maxVelocity; //The maximum velocity during telekinesis.
-    public Rigidbody2D rb;
+    private Rigidbody2D rb;
     private bool isBeingHeld = false;
     // Start is called before the first frame update
     void Start()
     {
+        gameObject.layer = 8;
         if(moveStrength == 0)
         {
             moveStrength = 2;
