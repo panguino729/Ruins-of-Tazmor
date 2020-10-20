d74db16a142ae9e30ed42f001b983c093c893823
al8249 <al8249@rit.edu>
2020-10-20 16:05:50 -0400

    Some form of package update
    Unkown change

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,24 +7,20 @@ using System.Runtime.CompilerServices;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private LayerMask platformLayerMask;
     public static PlayerMovement player;
     private Rigidbody2D rb;
-
-    private int jumpingCounter = 3;
-
+    private BoxCollider2D boxCollider2D;
     private bool jumping;
     Vector3 startPos;
-    public float maxSpeed = 1.0f;
-    public float jumpingForceMultiplier = 1.0f;
-    public float appliedForce = 1.0f;
-    public float jumpForce = 1.0f;
 
     void Start()
     {
         player = this;
         name = "Player";
         startPos = this.transform.position;
-        rb = GetComponent<Rigidbody2D>();
+        rb = transform.GetComponent<Rigidbody2D>();
+        boxCollider2D = transform.GetComponent<BoxCollider2D>();
         jumping = false;
     }
 
@@ -35,76 +31,36 @@ public class PlayerMovement : MonoBehaviour
 
     void ApplyForce()
     {
-        Vector2 uForce = new Vector2();
-
-        uForce += GetKeyDirection() * appliedForce;
-
-        //apply jumping impulse
-        if(GetJump())
-            uForce += new Vector2(0.0f, jumpForce);
-
-        //reduce horizontal force when jumping
-        if (jumping)
-            uForce.x *= jumpingForceMultiplier;
-        
-        rb.AddForce(uForce);
-
-        //cap speed
-        float speed = math.abs(rb.velocity.x);
-        if (speed > maxSpeed)
-            rb.velocity = new Vector2(speed / rb.velocity.x * maxSpeed , rb.velocity.y);
-    }
-
-    /// <summary>
-    /// get the direction of movement based on WASD or arrow keys
-    /// </summary>
-    Vector2 GetKeyDirection()
-    {
-        Vector2 direction = new Vector2();
+        float movespeed = 5f;
 
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            direction.x += 1;
+            rb.velocity = new Vector2(movespeed, rb.velocity.y);
         }
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
-            direction.x -= 1;
+            rb.velocity = new Vector2(-movespeed, rb.velocity.y);
+        }
+        else
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
         }
 
-        return direction;
+        if(IsGrounded() && Input.GetKey(KeyCode.Space))
+        {
+            float jumpV = 15f;
+            rb.velocity = Vector2.up * jumpV;
+        }
     }
 
-    /// <summary>
-    /// did the player just hit jump?
-    /// set jumping to appropriate value
-    /// </summary>
-    bool GetJump()
+    //
+    private bool IsGrounded()
     {
-        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            if (jumping == false)
-            {
-                jumping = true;
-                return true;
-            }
-        }
-        
-        if(rb.velocity.y == 0)
-        {
-            if (jumpingCounter != 0)
-            {
-                jumpingCounter -= 1;
-            }
-            else
-            {
-                jumping = false;
-                jumpingCounter = 3;
-            } 
-        }
-
-        return false;
+        RaycastHit2D raycastHit2D = Physics2D.BoxCast(boxCollider2D.bounds.center, boxCollider2D.bounds.size, 0f, Vector2.down, .1f, Physics2D.AllLayers, 0, 0);
+        return raycastHit2D.collider != null;
     }
 
+    //may be handled in collision manager later on, must be implemented
     private void OnCollisionEnter2D(Collision2D collision)
     {
         switch (collision.gameObject.tag)
