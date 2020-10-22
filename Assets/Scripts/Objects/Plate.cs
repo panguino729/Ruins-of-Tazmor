e4f1444154f9f2797d11b9c7513ce9b91f24cfee
Michael Hrehocik <mrh5766@rit.edu>
2020-10-22 17:49:29 -0400

    Update Plate.cs

--- Assets/Scripts/Objects/Plate.cs
+++ Assets/Scripts/Objects/Plate.cs
@@ -15,8 +15,6 @@ public class Plate : Object
     int framesSincePressed = 0;
     public float indentHeight; //The % height that the plate changes each frame upon being pressed - should probably be about 0.1
     GameObject collided;
-    public AudioSource source;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -42,30 +40,36 @@ public class Plate : Object
     {
         if (numPresses > 0 && transform.localScale.y > 0.5f * initialYScale)
         {
+            //Decreases the height of the plate
             gameObject.transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y - indentHeight * initialYScale, transform.localScale.z);
+            //Moves the plate down
             gameObject.transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y - indentHeight * initialHeight, transform.localPosition.z);
+            //Moves the collided object down
             collided.gameObject.transform.localPosition = new Vector3(collided.gameObject.transform.localPosition.x, collided.gameObject.transform.localPosition.y - indentHeight * initialHeight, collided.gameObject.transform.localPosition.z);
         }
         else if(numPresses <= 0 && transform.localScale.y < 1.0f * initialYScale && framesSincePressed > 5)
         {
+            //Increases the height of the plate
             gameObject.transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y + indentHeight * initialYScale, transform.localScale.z);
+            //Moves the plate up
             gameObject.transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y + indentHeight * initialHeight, transform.localPosition.z);
         }
-        if(numPresses == 0)
+        if(numPresses == 0) //Waits a few frames to move the plate back up so that it doesn't instantly get pressed again
         {
             framesSincePressed++;
         }
-        if (transform.localScale.y <= 0.5f * initialYScale && !pressed)
+        if(transform.localScale.y <= 0.5f * initialYScale)
         {
+            //When the plate is halfway down, it has been pressed
             pressed = true;
-            source.Play();
         }
-        else if (transform.localScale.y >= 1.0f * initialYScale)
+        else
         {
+            //If the plate is not halfway down, has not been pressed
             pressed = false;
         }
     }
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionEnter2D(Collision2D collision) //Increases the counter for objects pressing the plate when an object presses the plate
     {
         if(collision.gameObject.name.Contains("Moveable") || collision.gameObject.name.Contains("Player"))
         {
@@ -73,7 +77,7 @@ public class Plate : Object
             numPresses++;
         }
     }
-    private void OnCollisionExit2D(Collision2D collision)
+    private void OnCollisionExit2D(Collision2D collision) //Reduces the counter for objects pressing the plate when an object is no longer colliding with the plate
     {
         if (collision.gameObject.name.Contains("Moveable") || collision.gameObject.name.Contains("Player"))
         {
