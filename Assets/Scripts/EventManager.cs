0fd9853d70585db0afb48c2cca79bf87b59b8541
Van Edelman <vanedelman@gmail.com>
2020-10-22 15:19:23 -0400

    Added Event Manager

---
+++ Assets/Scripts/EventManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EventManager : MonoBehaviour
+{
+    public Scene nextLevel;
+    public List<Scene> levels;
+    private int currentLevel;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentLevel = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
